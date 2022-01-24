5c4a980fd79f90e3b529984459b4d33599ce48f1
tasherrill <74892440+tasherrill@users.noreply.github.com>
2022-01-24 12:37:33 -0500

    Fixed varible values

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -20,23 +20,23 @@ print("Searching for " + enter_one_character + " in " + enter_word)
 
 if enter_one_character == enter_word[0]:
     print(enter_one_character + " found at index 0")
-    num_match_character: int = num_match_character + 1
+    num_match_character = num_match_character + 1
 
 if enter_one_character == enter_word[1]:
     print(enter_one_character + " found at index 1")
-    num_match_character: int = num_match_character + 1
+    num_match_character = num_match_character + 1
 
 if enter_one_character == enter_word[2]:
     print(enter_one_character + " found at index 2")
-    num_match_character: int = num_match_character + 1
+    num_match_character = num_match_character + 1
 
 if enter_one_character == enter_word[3]:
     print(enter_one_character + " found at index 3")
-    num_match_character: int = num_match_character + 1
+    num_match_character = num_match_character + 1
 
 if enter_one_character == enter_word[4]:
     print(enter_one_character + " found at index 4")
-    num_match_character: int = num_match_character + 1
+    num_match_character = num_match_character + 1
 
 if num_match_character == 1:
     print(str(num_match_character) + " instance of " + enter_one_character + " found in " + enter_word)
